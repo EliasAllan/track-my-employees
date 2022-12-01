8c4f19f49f4b74d7f3b20ba2debd03d734615f46
AllanEl <allanrnelias@gmail.com>
2022-12-01 18:13:31 -0500

    add role functional

--- Develop/db/schema.sql
+++ Develop/db/schema.sql
@@ -5,18 +5,16 @@ USE employee_tracker_db;
 
 -- Add employee table --
 CREATE TABLE department(
-  `id` INT NOT NULL,
-  `name` VARCHAR(100) NOT NULL,
-PRIMARY KEY (id)
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(100) NOT NULL,
 );
 
 -- Add role table --
-CREATE TABLE `role` (
-  `id` INT ,
-  `title` VARCHAR(30) NOT NULL,
-  `salary` DECIMAL NOT NULL,
-  `department_id` INT,
-  PRIMARY KEY (id),
+CREATE TABLE role (
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INT,
   FOREIGN KEY (department_id)
   REFERENCES department(id)
   ON DELETE SET NULL
@@ -24,16 +22,16 @@ CREATE TABLE `role` (
 
 -- Add department table --
 CREATE TABLE employee(
-  `id` INT ,
-  `first_name` VARCHAR(30) NOT NULL,
-  `last_name` VARCHAR(30) NOT NULL,
-  `role_id` INT,
-  `manager_id` INT NOT NULL,
-  PRIMARY KEY (id),
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT,
+  manager_id INT,
   FOREIGN KEY (role_id)
-  REFERENCES `role`(id)
-  ON DELETE SET NULL
-  
+  REFERENCES role(id)
+  ON DELETE SET NULL,
+  FOREIGN KEY (manager_id)
+  REFERENCES employee(id)
 );
 
 -- SELECT * FROM role
